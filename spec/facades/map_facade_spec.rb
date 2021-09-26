2e2d0216aca879bb7b9a3b2c634356ff8d0d0b0f
Michael Abbott <michael@mjabbottdesign.com>
2021-09-26 12:39:57 -0700

    Create MapFacade ::lat_lng test

---
+++ spec/facades/map_facade_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe MapFacade do
+  context "class methods" do
+    context "::lat_lng" do
+      it "returns an array of Map objects", :vcr do
+        results = MapFacade.lat_lng('denver,co')
+
+        expect(results).to be_a(Map)
+      end
+    end
+  end
+end
