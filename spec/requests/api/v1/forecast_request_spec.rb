e0a347565e7841bf5d4a27dc4a073b26c6b16f4d
Michael Abbott <michael@mjabbottdesign.com>
2021-09-26 12:24:16 -0700

    Add vcr to forecast request test

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Forecast API' do
-  it 'sends weather data pertaining to a provided location' do
+  it 'sends weather data pertaining to a provided location', :vcr do
     get '/api/v1/forecast?location=denver,co'
 
     expect(response).to be_successful
