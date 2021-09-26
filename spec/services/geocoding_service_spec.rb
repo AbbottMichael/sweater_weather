07b5cdd8ca53aa835d0a5dba386632118afbef6a
Michael Abbott <michael@mjabbottdesign.com>
2021-09-25 23:08:05 -0700

    Update #get_lat_lng_by_location expect block to match json response

--- spec/services/geocoding_service_spec.rb
+++ spec/services/geocoding_service_spec.rb
@@ -5,16 +5,19 @@ RSpec.describe GeocodingService do
     context "#get_lat_lng_by_location" do
       it "returns the latitude and longitude of a given city and state" do
         search = GeocodingService.get_lat_lng_by_location("denver,co")
-        expect(search).to be_an Array
-        expect(search[:locations]).to be_an Array
-        location_data = search[:locations].first
+        expect(search).to be_a(Hash)
+        expect(search[:results]).to be_an(Array)
+        results = search[:results].first
+        expect(results).to have_key(:locations)
+        expect(results[:locations]).to be_an(Array)
+        location = results[:locations].first
 
-        expect(location_data).to have_key(:latLng)
-        expect(location_data[:latLng]).to be_a(Hash)
-        expect(location_data[:latLng]).to have_key(:lat)
-        expect(location_data[:latLng][:lat]).to be_a(Float)
-        expect(location_data[:latLng]).to have_key(:lng)
-        expect(location_data[:latLng][:lng]).to be_a(Float)
+        expect(location).to have_key(:latLng)
+        expect(location[:latLng]).to be_a(Hash)
+        expect(location[:latLng]).to have_key(:lat)
+        expect(location[:latLng][:lat]).to be_a(Float)
+        expect(location[:latLng]).to have_key(:lng)
+        expect(location[:latLng][:lng]).to be_a(Float)
       end
     end
   end
