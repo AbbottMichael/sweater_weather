db11be7556f8979d521335ff7fd5a90e0fc6cff1
Michael Abbott <michael@mjabbottdesign.com>
2021-09-25 23:32:22 -0700

    Refactor API key passed in ::conn

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -1,13 +1,14 @@
 class MapService
   def self.get_lat_lng_by_location(location)
     response = conn.get('/geocoding/v1/address') do |req|
-      req.params['key'] = ENV['mapquest_api_key']
       req.params['location'] = location
     end
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.conn
-    Faraday.new(url: 'http://www.mapquestapi.com')
+    Faraday.new(url: 'http://www.mapquestapi.com') do |f|
+      f.params['key'] = ENV['mapquest_api_key']
+    end
   end
 end
