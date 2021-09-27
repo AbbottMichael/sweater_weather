a261d03659ad7e9b0db89c437226be48997d2da7
Michael Abbott <michael@mjabbottdesign.com>
2021-09-27 10:23:48 -0700

    Add BookSearchFacade ::books_by_city test

--- spec/facades/book_search_facade_spec.rb
+++ spec/facades/book_search_facade_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe BookFacade do
+RSpec.describe BookSearchFacade do
   context "class methods" do
     context "::books_by_city" do
       it "returns book objects", :vcr do
-        coordinates = MapFacade.lat_lng(params[:location])
+        coordinates = MapFacade.lat_lng('denver,co')
         forecast = ForecastFacade.forecast_by_lat_lng(coordinates.lat, coordinates.lng)
-        results = ForecastFacade.books_by_city(forecast, 'denver,co', 5)
+        results = BookSearchFacade.books_by_city(forecast, 'denver,co', 5)
 
         expect(results).to be_a(Book)
       end
