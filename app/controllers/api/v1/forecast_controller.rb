52df4ad49e8e2703449a2a5af7ecff598c8be873
Michael Abbott <michael@mjabbottdesign.com>
2021-09-26 21:04:17 -0700

    Update ForecastController #index to call facade and serializer

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,5 +2,7 @@ class Api::V1::ForecastController < ApplicationController
 
   def index
     coordinates = MapFacade.lat_lng(params[:location])
+    forecast = ForecastFacade.forecast_by_lat_lng(coordinates.lat, coordinates.lng)
+    render json: ForecastSerializer.new(forecast)
   end
 end
