4b91d21f08ef25b5d20736e5dd205ed1367e69be
Michael Abbott <michael@mjabbottdesign.com>
2021-09-27 09:20:59 -0700

    Add location and weather to BookSearch controller

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::BookSearchController < ApplicationController
-  
-  def index
 
+  def index
+    coordinates = MapFacade.lat_lng(params[:location])
+    forecast = ForecastFacade.forecast_by_lat_lng(coordinates.lat, coordinates.lng)
+    #forecast.current_weather[:description]
+    #forecast.current_weather[:temperature]
   end
 end
