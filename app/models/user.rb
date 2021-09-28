32e1928cf9ed36b123b754a7114c1dd3531a0368
Michael Abbott <michael@mjabbottdesign.com>
2021-09-28 16:46:27 -0700

    Add User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
-  has_many :api_keys, as: :bearer 
+  has_many :api_keys, as: :bearer
+  
   has_secure_password
+
+  validates :password, confirmation: true
+  validates :email, uniqueness: true, presence: true, allow_empty: false
+  validates :password, presence: { require: true, allow_empty: false }
+  validates :password_confirmation, presence: { require: true, allow_empty: false }
+
 end
