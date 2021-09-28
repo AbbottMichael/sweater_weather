755c986406134f001df3ff285afdf286b1526f69
Michael Abbott <michael@mjabbottdesign.com>
2021-09-28 12:37:21 -0700

    Update UsersController #user_params, #create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,15 +2,18 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = user_params
-    user[:email] = user[:email].downcase!
+    user[:email] = user[:email].downcase
     new_user = User.new(user)
+
+    if new_user.save
+      api_key = new_user.api_keys.create! token: SecureRandom.hex
+      render json: UserSerializer.new(new_user), status: :created
+    end
   end
 
   private
 
   def user_params
-    params.
-    require(:user).
-    permit(:email, :password, :password_confirmation)
+    params.permit(:email, :password, :password_confirmation)
   end
 end
