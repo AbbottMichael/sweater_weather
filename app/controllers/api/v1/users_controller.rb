9aa379a47cd798fc955461d3fab114d28d8a515c
Michael Abbott <michael@mjabbottdesign.com>
2021-09-28 16:44:25 -0700

    Update UsersController #create and #user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,19 +1,44 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    user = user_params
-    user[:email] = user[:email].downcase
-    new_user = User.new(user)
-
+    new_user = User.new(user_params)
     if new_user.save
       api_key = new_user.api_keys.create! token: SecureRandom.hex
       render json: UserSerializer.new(new_user), status: :created
+    else
+      render json: {error: "#{new_user.errors.full_messages.to_sentence}"}, status: :bad_request
     end
   end
 
   private
 
   def user_params
-    params.permit(:email, :password, :password_confirmation)
+    return (render json: { error: 'Must send correct details in the body'}, status: :bad_request) if params[:user].nil?
+    user = params[:user]
+    user[:email] = user[:email].downcase if !user[:email].nil?
+    user[:password] = params[:password]
+    user[:password_confirmation] = params[:password_confirmation]
+    params.require(:user).
+    permit(:email, :password, :password_confirmation)
   end
+
+  # def user_params
+  #   x = ActionDispatch::Request.new(response)
+  #   params.require(:user).
+  #   permit(:email, :password, :password_confirmation)
+  # end
+
+  # def user_params
+  #   # (render json: { error: 'Must send correct details in the body'}, status: :bad_request)
+  #   return if JSON.parse(params[:_json])
+  #   params[:body] = JSON.parse(params[:_json])
+  #   params.require(:body).
+  #   permit(:email, :password, :password_confirmation)
+  # end
+
+  # def user_params
+  #   # params.require(:_json).
+  #   params.permit(:email, :password, :password_confirmation)
+  # end
+
 end
