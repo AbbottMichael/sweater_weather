5154e65feaf383a79c0594ece6fd86fd20256314
Michael Abbott <michael@mjabbottdesign.com>
2021-09-28 16:38:10 -0700

    Add UsersRequest sad path tests

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe 'users API' do
     post '/api/v1/users', params: body, as: :json
 
     expect(response).to be_successful
+    expect(response.status).to eq(201)
 
     user = JSON.parse(response.body, symbolize_names: true)
 
@@ -26,5 +27,70 @@ RSpec.describe 'users API' do
     expect(user[:data][:attributes][:email]).to eq(body[:email].downcase)
     expect(user[:data][:attributes]).to have_key(:api_key)
     expect(user[:data][:attributes][:email]).to be_a(String)
+  end
+
+  it 'returns an error if email is missing', :vcr do
+    body = {
+      password: "password",
+      password_confirmation: "password"
+    }
+
+    post '/api/v1/users', params: body, as: :json
+
+    expect(response.status).to eq(400)
+
+    message = JSON.parse(response.body, symbolize_names: true)
+
+    expect(message).to have_key(:error)
+    expect(message[:error]).to eq("Email can't be blank")
+  end
+
+  it 'returns an error if password is missing', :vcr do
+    body = {
+      email: "Whatever@example.com",
+      password_confirmation: "password"
+    }
+
+    post '/api/v1/users', params: body, as: :json
+
+    expect(response.status).to eq(400)
+
+    message = JSON.parse(response.body, symbolize_names: true)
+
+    expect(message).to have_key(:error)
+    expect(message[:error]).to eq("Password can't be blank and Password confirmation doesn't match Password")
+  end
+
+  it 'returns an error if password_confirmation is missing', :vcr do
+    body = {
+      email: "Whatever@example.com",
+      password: "password"
+    }
+
+    post '/api/v1/users', params: body, as: :json
+
+    expect(response.status).to eq(400)
+
+    message = JSON.parse(response.body, symbolize_names: true)
+
+    expect(message).to have_key(:error)
+    expect(message[:error]).to eq("Password confirmation can't be blank")
+  end
+
+  it 'returns an error if password and confirmation do not match', :vcr do
+    body = {
+      email: "Whatever@example.com",
+      password: "password",
+      password_confirmation: "what?"
+    }
+
+    post '/api/v1/users', params: body, as: :json
+
+    expect(response.status).to eq(400)
+
+    message = JSON.parse(response.body, symbolize_names: true)
+
+    expect(message).to have_key(:error)
+    expect(message[:error]).to eq("Password confirmation doesn't match Password")
   end
 end
