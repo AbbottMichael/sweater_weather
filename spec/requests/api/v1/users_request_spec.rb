522d4cd1a1aa81760adf0f8263a3a294c3d32b6a
Michael Abbott <michael@mjabbottdesign.com>
2021-09-28 12:20:48 -0700

    Add response expect statements in UsersRequest test

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -9,5 +9,22 @@ RSpec.describe 'users API' do
     post '/api/v1/users', params: body, as: :json
 
     expect(response).to be_successful
+
+    user = JSON.parse(response.body, symbolize_names: true)
+
+    expect(user).to have_key(:data)
+    expect(user[:data]).to be_a(Hash)
+    expect(user[:data].keys.count).to eq(3)
+    expect(user[:data]).to have_key(:id)
+    expect(user[:data][:id]).to be_a(String)
+    expect(user[:data]).to have_key(:type)
+    expect(user[:data][:type]).to eq("user")
+    expect(user[:data]).to have_key(:attributes)
+    expect(user[:data][:attributes]).to be_a(Hash)
+    expect(user[:data][:attributes].keys.count).to eq(2)
+    expect(user[:data][:attributes]).to have_key(:email)
+    expect(user[:data][:attributes][:email]).to eq(body[:email].downcase)
+    expect(user[:data][:attributes]).to have_key(:api_key)
+    expect(user[:data][:attributes][:email]).to be_a(String)
   end
 end
