82e350d485c56b8a6ecb727dacf0c16cb34ce379
Michael Abbott <michael@mjabbottdesign.com>
2021-09-28 21:00:04 -0700

    Add a begin-rescue blcok to SessionsController #create

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
-    if session_params[:email].nil? || session_params[:password].nil?
-      return (render json: {error: "Must provide an email and a password"}, status: :bad_request)
+    begin
+      if session_params[:email].nil? || session_params[:password].nil?
+        return (render json: {error: "Must provide an email and a password"}, status: :bad_request)
+      end
+    rescue => e
+      return (render json: {error: "Must provide login details in the body"}, status: :bad_request)
     end
     user = User.find_by(email: session_params[:email].downcase)
     if user.nil?
