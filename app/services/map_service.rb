ecbf6ff2259d152c5e1247480149b2bd2ef3cfc0
Michael Abbott <michael@mjabbottdesign.com>
2021-09-29 03:14:46 -0700

    Add MapService ::get_trip_details

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -6,6 +6,14 @@ class MapService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.get_trip_details(origin, destination)
+    response = conn.get('/directions/v2/route') do |req|
+      req.params['from'] = origin
+      req.params['to'] = destination
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.conn
     Faraday.new(url: 'http://www.mapquestapi.com') do |f|
       f.params['key'] = ENV['mapquest_api_key']
