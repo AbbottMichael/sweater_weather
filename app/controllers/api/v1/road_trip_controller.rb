7b1eb6f890c39b05a17f220c25723e4a0b3a4bb5
Michael Abbott <michael@mjabbottdesign.com>
2021-09-29 03:15:35 -0700

    Refactor params verification to #params_verify RoatTripController

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -5,6 +5,7 @@ class Api::V1::RoadTripController < ApplicationController
     coordinates = MapFacade.lat_lng(trip_params[:destination])
     forecast = ForecastFacade.forecast_by_lat_lng(coordinates.lat, coordinates.lng)
     trip_info = RoadTripFacade.trip_details(trip_params[:origin], trip_params[:destination], forecast)
+    render json: RoadTripSerializer.new(trip_info)
   end
 
   private
@@ -14,7 +15,18 @@ class Api::V1::RoadTripController < ApplicationController
       permit(:origin, :destination, :api_key)
   end
 
+  def params_verify
+    begin
+      if trip_params[:origin].nil? || trip_params[:destination].nil? || trip_params[:api_key].nil?
+        return (render json: {error: "Must provide an origin and a destination"}, status: :bad_request)
+      end
+    rescue => e
+      return (render json: {error: "Must provide input in the body"}, status: :bad_request)
+    end
+  end
+
   def authenticator
+    params_verify
     auth = ApiKey.find_by(token: trip_params[:api_key])
     return (render json:
       { error: "Not authorized. Please provide a valid API key" },
